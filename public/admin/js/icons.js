c07e169c503f93411303d48050b83af092e384ae
Imanuel Ulbricht <me@imanuel.dev>
2021-08-12 00:35:15 +0200

    #14 Rewrite icon settings
    Rewrote icon settings

---
+++ public/admin/js/icons.js
@@ -0,0 +1,41 @@
+import {compileTemplate} from "./template.js";
+import {unmarkSubMenuLinks} from "./navigation.js";
+
+export async function init() {
+    unmarkSubMenuLinks();
+    document.querySelector('[data-sublink=icons]').classList.add('cosmo-menu-bar__sub-item--active');
+
+    await compileTemplate('icons.hbs', document.getElementById('rcContent'));
+
+    const touchiconInput = document.querySelector('#touchicon');
+    touchiconInput.addEventListener('change', (e) => {
+        const target = e.target;
+        document.querySelector('[for=touchicon].cosmo-picker__name').textContent = target.files.item(0).name;
+    });
+
+    const logoInput = document.querySelector('#logo');
+    logoInput.addEventListener('change', (e) => {
+        const target = e.target;
+        document.querySelector('[for=logo].cosmo-picker__name').textContent = target.files.item(0).name;
+    });
+
+    const faviconInput = document.querySelector('#favicon');
+    faviconInput.addEventListener('change', (e) => {
+        const target = e.target;
+        document.querySelector('[for=favicon].cosmo-picker__name').textContent = target.files.item(0).name;
+    });
+
+    document.querySelector('form').addEventListener('submit', async (e) => {
+        e.preventDefault();
+        if (faviconInput.files.length > 0) {
+            await fetch('/admin/settings/favicon', {method: 'POST', body: faviconInput.files.item(0)});
+        }
+        if (touchiconInput.files.length > 0) {
+            await fetch('/admin/settings/touchicon', {method: 'POST', body: touchiconInput.files.item(0)});
+        }
+        if (logoInput.files.length > 0) {
+            await fetch('/admin/settings/logo', {method: 'POST', body: logoInput.files.item(0)});
+        }
+        await compileTemplate('icons.hbs', document.getElementById('rcContent'));
+    });
+}
