2335e1f9ab1f1ad344ec4ddc9b740a2ebff1f585
Imanuel Ulbricht <me@imanuel.dev>
2022-05-01 16:47:10 +0200

    Removed news and downloads

--- frontend/router.go
+++ frontend/router.go
@@ -15,24 +15,6 @@ func InitRouting(mux *http.ServeMux) error {
 	mux.HandleFunc("/download/file/", routes.DownloadFile)
 	mux.HandleFunc("/download/preview/", routes.DownloadPreview)
 
-	mux.HandleFunc("/de/news", middleware.ErrorHandlerMiddleware(middleware.LanguageDetectorMiddleware(routes.NewsList)))
-	mux.HandleFunc("/en/news", middleware.ErrorHandlerMiddleware(middleware.LanguageDetectorMiddleware(routes.NewsList)))
-	mux.HandleFunc("/news", func(w http.ResponseWriter, r *http.Request) {
-		http.Redirect(w, r, "/en/news", http.StatusFound)
-	})
-
-	mux.HandleFunc("/de/news/", middleware.ErrorHandlerMiddleware(middleware.LanguageDetectorMiddleware(routes.NewsPage)))
-	mux.HandleFunc("/en/news/", middleware.ErrorHandlerMiddleware(middleware.LanguageDetectorMiddleware(routes.NewsPage)))
-	mux.HandleFunc("/news/", func(w http.ResponseWriter, r *http.Request) {
-		http.Redirect(w, r, "/en"+r.URL.Path, http.StatusFound)
-	})
-
-	mux.HandleFunc("/de/download", middleware.ErrorHandlerMiddleware(middleware.LanguageDetectorMiddleware(routes.DownloadList)))
-	mux.HandleFunc("/en/download", middleware.ErrorHandlerMiddleware(middleware.LanguageDetectorMiddleware(routes.DownloadList)))
-	mux.HandleFunc("/download", func(w http.ResponseWriter, r *http.Request) {
-		http.Redirect(w, r, "/en/download", http.StatusFound)
-	})
-
 	mux.HandleFunc("/de/download/", middleware.ErrorHandlerMiddleware(middleware.LanguageDetectorMiddleware(routes.DownloadPage)))
 	mux.HandleFunc("/en/download/", middleware.ErrorHandlerMiddleware(middleware.LanguageDetectorMiddleware(routes.DownloadPage)))
 	mux.HandleFunc("/download/", func(w http.ResponseWriter, r *http.Request) {
@@ -64,13 +46,13 @@ func InitRouting(mux *http.ServeMux) error {
 	})
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.Redirect(w, r, "/en/news", http.StatusFound)
+		http.Redirect(w, r, "/en/profile", http.StatusFound)
 	})
 	mux.HandleFunc("/en", func(w http.ResponseWriter, r *http.Request) {
-		http.Redirect(w, r, "/en/news", http.StatusFound)
+		http.Redirect(w, r, "/en/profile", http.StatusFound)
 	})
 	mux.HandleFunc("/de", func(w http.ResponseWriter, r *http.Request) {
-		http.Redirect(w, r, "/de/news", http.StatusFound)
+		http.Redirect(w, r, "/de/profile", http.StatusFound)
 	})
 
 	mux.HandleFunc("/en/", middleware.ErrorHandlerMiddleware(routes.Error404Handler))
